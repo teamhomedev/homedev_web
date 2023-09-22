f0c28597c87817023b3ee6df0fd4ec5cbb153434
Ly Nguyen <contact@minhlynguyenvo.com>
2023-09-22 20:30:42 +0200

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React, {useEffect} from 'react';
+import React, {PropsWithChildren, useEffect} from 'react';
 import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
 import {Home} from "./page/Home";
 import {Auth} from "./page/Auth";
 
 export function App() {
+    return (
+        <BrowserRouter>
+            <AppShell>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+
+                    <Route path="/auth/instaget" element={<Auth />} />
+                </Routes>
+            </AppShell>
+        </BrowserRouter>
+    )
+}
+
+function AppShell({children}: PropsWithChildren) {
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -12,19 +26,9 @@ export function App() {
             localStorage.removeItem('path');
             navigate(path);
         }
-
-        console.log('test')
     }, []);
 
     return (
-        <BrowserRouter>
-
-                <Routes>
-                    <Route path="/" element={<Home />} />
-
-                    <Route path="/auth/instaget" element={<Auth />} />
-
-                </Routes>
-        </BrowserRouter>
+        <div>{children}</div>
     )
 }
